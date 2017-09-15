ce63b356efb3dd3e45ededb1bb4ab7571ff21811
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-09-15 14:39:03 +0100

    HTML tab title set, Ajax requests written

---
+++ front_end/src/services/ajax_request.js
@@ -0,0 +1,30 @@
+var AjaxRequest = function(url) {
+    this.url = url;
+    this.data = [];
+}
+
+AjaxRequest.prototype.get = function(callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', this.url);
+    request.addEventListener('load', function() {
+        if (request.status !== 200) return;
+        var jsonString = request.responseText;
+        this.data = JSON.parse(jsonString);
+        callback(this.data);
+    }.bind(this));
+    request.send();
+}
+
+AjaxRequest.prototype.post = function(data) {
+    var request = new XMLHttpRequest();
+    request.open("POST", this.url);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.addEventListener('load', function() {
+        if (request.status!==200) return;
+        var jsonString = request.responseText;
+        this.data = JSON.parse(jsonString);
+    }.bind(this));
+request.send(JSON.stringify(data));
+}
+
+module.exports = AjaxRequest;
