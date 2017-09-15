3528486d3b89f7cdb8282e1a9c84b9f3e4b44414
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-09-15 16:49:38 +0100

    Added front end server

---
+++ front_end/server.js
@@ -0,0 +1,12 @@
+var express = require( 'express' );
+var server = express();
+var router = express.Router();
+server.use( express.static( 'build' ) );
+
+server.listen( 3000, function(){
+  console.log( "Front end server running on port 3000" );
+});
+
+router.get( '/', function( req, res ){
+  res.sendFile( __dirname + '/index.html' );
+});
