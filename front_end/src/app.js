d1ab3b223e6798773174cdebae8a0899c47a75bd
ben-ghirardani <pretty.fiendish@gmail.com>
2017-09-15 15:38:47 +0100

    new PortfolioView call in app.js

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -2,7 +2,7 @@ var AjaxRequest = require('/services/ajax_request.js');
 var PortfolioView = require('/views/portfolio_view.js');
 
 function app() {
-
+    var portfolioView = new PortfolioView();
 }
 
 window.addEventListener('load', app);
