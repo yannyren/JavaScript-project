d69323ad7fdcd98063d7c2a262b2ef9dee7ab056
ben-ghirardani <pretty.fiendish@gmail.com>
2017-09-15 15:49:10 +0100

    linked AjaxRequest to PortfolioView in app.js

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -3,6 +3,8 @@ var PortfolioView = require('/views/portfolio_view.js');
 
 function app() {
     var portfolioView = new PortfolioView();
+    var seedData = new AjaxRequest("http://localhost:3001/");
+    seedData.get(portfolioView.render);
 }
 
 window.addEventListener('load', app);
