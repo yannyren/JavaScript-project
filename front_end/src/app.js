9f172cbf7214544ff5e2208cce9e9c4884458cab
ben-ghirardani <pretty.fiendish@gmail.com>
2017-09-16 12:42:57 +0100

    begun work on a newsFeedView

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -1,10 +1,16 @@
 var AjaxRequest = require('/services/ajax_request.js');
 var PortfolioView = require('/views/portfolio_view.js');
+var NewsFeedView = require('/views/portfolio_view.js');
 
 function app() {
     var portfolioView = new PortfolioView();
     var seedData = new AjaxRequest("http://localhost:3001/");
     seedData.get(portfolioView.render);
+
+    // docs are quite the saga to go through...
+    var newsFeedView = new NewsFeedView();
+    var newsItems = new AjaxRequest( link to the Twitter API here );
+    newsItems.get(newsFeedView.render);
 }
 
 window.addEventListener('load', app);
