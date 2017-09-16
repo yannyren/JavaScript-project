83af84d57408e0b7f6baca5903663faf255d487d
ben-ghirardani <pretty.fiendish@gmail.com>
2017-09-16 14:24:30 +0100

    added event listeners for switch between overview and details pages

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -2,17 +2,24 @@ var portfolioView = new PortfolioView();
 var seedData = new AjaxRequest("http://localhost:3001/");
 seedData.get(portfolioView.render);
 
+
+var app = function(){
+
 var overviewbtn = document.getElementById('overviewbtn');
 overviewbtn.addEventListener('click', function() {
-
+    var overviewPage = document.getElementById('overviewpage');
+    overviewPage.style.display = 'block';
+    var detailsPage = document.getElementById('detailspage');
+    detailsPage.style.display = 'none';
 })
 
 var detailsbtn = document.getElementById('detailsbtn')
-
 detailsbtn.addEventListener('click', function() {
-
+    var detailsPage = document.getElementById('detailspage');
+    detailsPage.style.display = 'block';
+    var overviewPage = document.getElementById('overviewpage');
+    overviewPage.style.display = 'none';
 })
-
 
 }
 
