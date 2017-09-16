eb928b44819b5346c05d773a578ac67fd2694d1c
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-09-16 13:05:08 +0100

    Added update, delete, destroy on /api/portfolio

--- back_end/controllers/index.js
+++ back_end/controllers/index.js
@@ -2,6 +2,6 @@ var express = require( 'express' );
 var router = express.Router();
 var path = require( 'path' );
 
-router.use( '/api/shares', require( './shares' ) );
+router.use( '/api/portfolio', require( './portfolio' ) );
 
 module.exports = router;
