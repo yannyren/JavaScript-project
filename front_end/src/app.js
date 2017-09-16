ef1795e412a0a912d906d37b1b27cadd87fca288
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-09-16 12:48:08 +0100

    Event listeners added on nav buttons - page sections set up

--- front_end/src/app.js
+++ front_end/src/app.js
@@ -5,6 +5,17 @@ function app() {
     var portfolioView = new PortfolioView();
     var seedData = new AjaxRequest("http://localhost:3001/");
     seedData.get(portfolioView.render);
+
+
+    var overviewbtn = document.getElementById('overviewbtn');
+    overviewbtn.addEventListener('click', function() {
+
+    })
+
+    var detailsbtn = document.getElementById('detailsbtn');
+    detailsbtn.addEventListener('click', function() {
+        
+    })
 }
 
 window.addEventListener('load', app);
