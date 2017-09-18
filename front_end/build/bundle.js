7189d6b696b3ece5187d48453aac3da08c50f43d
ben-ghirardani <pretty.fiendish@gmail.com>
2017-09-18 14:59:09 +0100

    two line list not going to work

--- front_end/build/bundle.js
+++ front_end/build/bundle.js
@@ -118,13 +118,13 @@ var PortfolioView = function(){
 }
 
 PortfolioView.prototype.render = function(portfolioData){
+    console.log( "should be portfolioData", portfolioData); 
     var portfolioList = document.getElementById('portfolio-list');
-    console.log( "should be portfolioList", portfolioList); 
     for (var i = 0; i < portfolioData.length; i++) {
-        var listItem = document.createElement('li');
-        listItem.value = i;
-        listItem.innerText = portfolioData[i].name;
-        portfolioList.appendChild(listItem);
+        var option = document.createElement('option');
+        option.value = i;
+        option.innerText = portfolioData[i].name;
+        portfolioList.appendChild(option);
     }
 }
 
