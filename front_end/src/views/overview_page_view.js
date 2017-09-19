76287fb8e3d771aead8a8873511ad8e97e654ef8
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-09-19 14:20:12 +0100

    valuation function added

--- front_end/src/views/overview_page_view.js
+++ front_end/src/views/overview_page_view.js
@@ -1,4 +1,5 @@
 var PieChart = require( './portfolioPieChart_view');
+var Valuation = require( './valuation_view');
 
 var OverviewPage = function( refresh ) {
     this.data = null;
@@ -7,11 +8,15 @@ var OverviewPage = function( refresh ) {
     //grab dom elements
     pieChartContainer = document.querySelector( '#pieChart' );
     pieChart = new PieChart( this.refresh, pieChartContainer );
+    totalValuation = document.querySelector( '#valuation');
+    valuation = new Valuation (this.refresh, totalValuation);
 }
 
 OverviewPage.prototype.render = function(){
     pieChart.setData( this.data );
     pieChart.render();
+    valuation.setData( this.data);
+    valuation.render();
 }
 
 OverviewPage.prototype.setData = function( data ){
