41f6e3a73e5f2eb99fed52054b8990255b6d94e5
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-09-20 11:30:10 +0100

    portfolio stock line values added

--- front_end/src/views/overview_page_view.js
+++ front_end/src/views/overview_page_view.js
@@ -1,5 +1,6 @@
 var PieChart = require( './portfolioPieChart_view');
 var Valuation = require( './valuation_view');
+var SingleLineValuation = require( './single_stock_line_valuation');
 
 var OverviewPage = function( refresh ) {
     this.data = null;
@@ -10,6 +11,9 @@ var OverviewPage = function( refresh ) {
     pieChart = new PieChart( this.refresh, pieChartContainer );
     totalValuation = document.querySelector( '#valuation');
     valuation = new Valuation (this.refresh, totalValuation);
+    singleLineValuesContainer = document.querySelector('#singlelinevaluations');
+    console.log(singleLineValuesContainer);
+    singleLineValuesData = new SingleLineValuation(this.refresh, singleLineValuesContainer);
 }
 
 OverviewPage.prototype.render = function(){
@@ -17,6 +21,8 @@ OverviewPage.prototype.render = function(){
     pieChart.render();
     valuation.setData( this.data);
     valuation.render();
+    singleLineValuesData.setData(this.data);
+    singleLineValuesData.render();
 }
 
 OverviewPage.prototype.setData = function( data ){
