4289c4d4da727bcb8de6b80e3187162bba704ab2
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-09-21 09:35:12 +0100

    Minor bug fix

--- back_end/db/portfolio_seeds_sample.js
+++ back_end/db/portfolio_seeds_sample.js
@@ -10,7 +10,7 @@ db.portfolio.insert(
       "price": 120.00,
       "quantity": 2000,
       "buyPrice": 80.00,
-      "lastUpdated": "2017-09-20",
+      "lastUpdated": "2017-09-21",
       "pastCloseOfDayPrices": [92.00, 89.00, 103.00, 125.00, 108.00, 98.00, 110.00],
       "buyDate":"2014-11-15"
     },
@@ -20,7 +20,7 @@ db.portfolio.insert(
       "price": 100.00,
       "quantity": 1000,
       "buyPrice": 60.00,
-      "lastUpdated": "2017-09-20",
+      "lastUpdated": "2017-09-21",
       "pastCloseOfDayPrices": [72.00, 49.00, 83.00, 85.00, 88.00, 88.00, 110.00],
       "buyDate":"2014-11-15"
     },
@@ -30,7 +30,7 @@ db.portfolio.insert(
       "price": 110.00,
       "quantity": 1000,
       "buyPrice": 60.00,
-      "lastUpdated": "2017-09-20",
+      "lastUpdated": "2017-09-21",
       "pastCloseOfDayPrices": [42.00, 59.00, 63.00, 75.00, 48.00, 58.00, 70.00],
       "buyDate":"2014-11-15"
     },
@@ -40,7 +40,7 @@ db.portfolio.insert(
       "price": 110.00,
       "quantity": 1000,
       "buyPrice": 60.00,
-      "lastUpdated": "2017-09-20",
+      "lastUpdated": "2017-09-21",
       "pastCloseOfDayPrices": [42.00, 79.00, 63.00, 65.00, 48.00, 38.00, 40.00],
       "buyDate":"2014-11-15"
     },
@@ -50,7 +50,7 @@ db.portfolio.insert(
       "price": 110.00,
       "quantity": 1000,
       "buyPrice": 60.00,
-      "lastUpdated": "2017-09-20",
+      "lastUpdated": "2017-09-21",
       "pastCloseOfDayPrices": [52.00, 69.00, 63.00, 55.00, 68.00, 58.00, 70.00],
       "buyDate":"2014-11-15"
     }
