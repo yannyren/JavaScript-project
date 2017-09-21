527aa30b1e9682982bf66c09a5bc00375ea77a7f
Derek Middlemiss <derekmiddlemiss@gmail.com>
2017-09-21 09:07:10 +0100

    Changed background colour of total valuation - too dark to read

--- back_end/db/portfolio_seeds_sample.js
+++ back_end/db/portfolio_seeds_sample.js
@@ -31,7 +31,7 @@ db.portfolio.insert(
       "quantity": 1000,
       "buyPrice": 60.00,
       "lastUpdated": "2017-09-20",
-      "pastCloseOfDayPrices": [42.00, 79.00, 63.00, 65.00, 48.00, 38.00, 210.00],
+      "pastCloseOfDayPrices": [42.00, 59.00, 63.00, 75.00, 48.00, 58.00, 70.00],
       "buyDate":"2014-11-15"
     },
     {
@@ -41,7 +41,7 @@ db.portfolio.insert(
       "quantity": 1000,
       "buyPrice": 60.00,
       "lastUpdated": "2017-09-20",
-      "pastCloseOfDayPrices": [42.00, 79.00, 63.00, 65.00, 48.00, 38.00, 210.00],
+      "pastCloseOfDayPrices": [42.00, 79.00, 63.00, 65.00, 48.00, 38.00, 40.00],
       "buyDate":"2014-11-15"
     },
     {
@@ -51,7 +51,7 @@ db.portfolio.insert(
       "quantity": 1000,
       "buyPrice": 60.00,
       "lastUpdated": "2017-09-20",
-      "pastCloseOfDayPrices": [42.00, 79.00, 63.00, 65.00, 48.00, 38.00, 210.00],
+      "pastCloseOfDayPrices": [52.00, 69.00, 63.00, 55.00, 68.00, 58.00, 70.00],
       "buyDate":"2014-11-15"
     }
   ]
